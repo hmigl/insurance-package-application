fd7fd96763669d9c0f944b6d423be42b460db150
hmigl <88746406+hmigl@users.noreply.github.com>
2023-02-17 10:56:24 -0300

    feat: first commit

---
+++ src/main/java/com/hmigl/insurancepackage/InsurancePackageApplication.java
@@ -0,0 +1,12 @@
+package com.hmigl.insurancepackage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsurancePackageApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(InsurancePackageApplication.class, args);
+  }
+}
