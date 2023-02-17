af8af1648c387ef9f3d0f28394d2ac4f023f53a0
hmigl <88746406+hmigl@users.noreply.github.com>
2023-02-17 18:14:32 -0300

    refactor: make 'score' a private field for each enum

--- src/main/java/com/hmigl/insurancepackage/domain/Score.java
+++ src/main/java/com/hmigl/insurancepackage/domain/Score.java
@@ -4,74 +4,67 @@ import com.hmigl.insurancepackage.web.UserInformation;
 
 public enum Score {
   AUTO {
+    private String score;
+
     @Override
     public String calculateScore(UserInformation userInformation, int baseScore) {
-      String score;
-
       baseScore -= AUTO.deductRiskPointBasedOnAge(userInformation.age());
       baseScore -= AUTO.deductRiskPointBasedOnIncome(userInformation.income());
-
       if (userInformation.vehicle() != null
           && userInformation.vehicle().year() >= userInformation.vehicle().year() - 5)
         baseScore += 1;
 
       score = AUTO.getScore(baseScore);
-
       if (userInformation.vehicle() == null) score = "ineligible";
 
       return score;
     }
   },
   DISABILITY {
+    private String score;
+
     @Override
     public String calculateScore(UserInformation userInformation, int baseScore) {
-      String score;
-
       baseScore -= DISABILITY.deductRiskPointBasedOnAge(userInformation.age());
       baseScore -= DISABILITY.deductRiskPointBasedOnIncome(userInformation.income());
       baseScore += DISABILITY.addRiskPointBasedOnDependents(userInformation.dependents());
       baseScore -= DISABILITY.addOrDeductRiskPointBasedOnMaritalStatus(userInformation.maritalStatus());
-
       if (userInformation.house() != null
           && userInformation.house().ownershipStatus().equals("mortgaged")) baseScore += 1;
 
       score = DISABILITY.getScore(baseScore);
-
       if (userInformation.income() == 0 || userInformation.age() >= 60) score = "ineligible";
 
       return score;
     }
   },
   HOME {
+    private String score;
+
     @Override
     public String calculateScore(UserInformation userInformation, int baseScore) {
-      String score;
-
       baseScore -= HOME.deductRiskPointBasedOnAge(userInformation.age());
       baseScore -= HOME.deductRiskPointBasedOnIncome(userInformation.income());
-
       if (userInformation.house() != null
           && userInformation.house().ownershipStatus().equals("mortgaged")) baseScore += 1;
 
       score = HOME.getScore(baseScore);
-
       if (userInformation.house() == null) score = "ineligible";
 
       return score;
     }
   },
   LIFE {
+    private String score;
+
     @Override
     public String calculateScore(UserInformation userInformation, int baseScore) {
-      String score;
-
       baseScore -= LIFE.deductRiskPointBasedOnAge(userInformation.age());
       baseScore -= LIFE.deductRiskPointBasedOnIncome(userInformation.income());
       baseScore += LIFE.addRiskPointBasedOnDependents(userInformation.dependents());
       baseScore += LIFE.addOrDeductRiskPointBasedOnMaritalStatus(userInformation.maritalStatus());
 
       score = LIFE.getScore(baseScore);
-
       if (userInformation.age() >= 60) score = "ineligible";
 
       return score;
